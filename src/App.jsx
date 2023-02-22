54ae953a2365f0c983cdd7f62935b96300945998
joaquin <floresedgarjoaquin@gmail.com>
2023-02-21 19:07:58 -0600

    First commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,34 @@
-import { ApolloProvider } from "@apollo/client";
-import { Box, Text } from "@chakra-ui/react";
-import { useState } from "react";
-import client from "./config/apollo.js";
-import { AuthPage } from "./pages/AuthPage.jsx";
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './App.css'
 
-export const App = () => {
-  const [auth, setauthAuth] = useState(undefined);
+function App() {
+  const [count, setCount] = useState(0)
 
   return (
-    <ApolloProvider client={client}>
-        {!auth ? <AuthPage /> : <Text as="h1"> Estas Logeado </Text>}
-    </ApolloProvider>
-  );
-};
+    <div className="App">
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src="/vite.svg" className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://reactjs.org" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </div>
+  )
+}
+
+export default App
--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,10 @@
-import { ChakraProvider } from "@chakra-ui/react";
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { App } from "./App";
-import { theme } from "./theme/theme";
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
+import './index.css'
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <ChakraProvider theme={theme}>
-      <App />
-    </ChakraProvider>
-  </React.StrictMode>
-);
+    <App />
+  </React.StrictMode>,
+)
