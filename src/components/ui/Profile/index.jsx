3b73ebb922c52857d2cde1fc4eea1a6f610dd0a1
joaquin <floresedgarjoaquin@gmail.com>
2023-03-04 20:59:57 -0600

    Dev: Obteniendo datos de usuario desde backend para <UserPage/>

--- src/components/ui/Profile/index.jsx
+++ src/components/ui/Profile/index.jsx
@@ -7,12 +7,15 @@ import { GET_USER } from '../../../gql/user'
 
 export const Profile = ({ username }) => {
 
-    const result = useQuery(GET_USER,{
+    const { data, loading, error } = useQuery(GET_USER,{
         variables: { username }
     })
+        if(loading) return <Text fontSize='3xl'> Loading... </Text>
+        if(error) return <Text fontSize='3xl'>Usuario no encontrado!</Text>
+        const { getUser } = data;
 
-    console.log(result);
-
+        console.log(getUser);
+        
     return (
         <Container centerContent>
             <Text as='h1' fontSize='6xl'> { username }</Text>
