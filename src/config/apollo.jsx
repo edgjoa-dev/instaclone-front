660ca5041a574ce9f42c0c77860105bd7a21a765
joaquin <floresedgarjoaquin@gmail.com>
2023-03-25 17:46:19 -0600

    Dev: Continuación con mutation para avatar todo ok

--- src/config/apollo.jsx
+++ src/config/apollo.jsx
@@ -1,9 +1,15 @@
 import { ApolloClient, InMemoryCache } from '@apollo/client'
+import { createUploadLink } from 'apollo-upload-client'
+
+
+const httpLink = createUploadLink({
+    uri: 'http://localhost:4000/graphql',
+})
 
 const client = new ApolloClient({
     connectToDevTools: true,
     cache: new InMemoryCache(),
-    uri:'http://localhost:4000'
+    link: httpLink
 })
 
 
